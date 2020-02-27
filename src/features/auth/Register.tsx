f9e827fe15a47219cf3966a70ed64d07cabcaef6
4ortytwo <burgan.arthur@gmail.com>
2020-02-27 22:33:06 +0100

    center Register comp

--- src/features/auth/Register.tsx
+++ src/features/auth/Register.tsx
@@ -1,17 +1,17 @@
 import React from "react";
 import { View, Text, Button } from "react-native";
 import { NavProps } from "../../ParamList";
-
+import { Center } from "../Center";
 interface RegisterProps extends NavProps<"Register"> {}
 
 export const Register: React.FC<RegisterProps> = ({ navigation, route }) => {
   return (
-    <View>
+    <Center>
       <Text>Register</Text>
       <Button
         title="Go to login"
         onPress={() => navigation.navigate("Login")}
       />
-    </View>
+    </Center>
   );
 };
