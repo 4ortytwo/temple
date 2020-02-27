85075aee0bda4c51e445b44040054d8f11ce0cbf
4ortytwo <burgan.arthur@gmail.com>
2020-02-27 15:23:19 +0100

    use store in albums

--- src/features/albums/Albums.tsx
+++ src/features/albums/Albums.tsx
@@ -1,9 +1,46 @@
 import React, { useEffect } from "react";
-import { View, Text } from "react-native";
-
+import { View, Text, ActivityIndicator } from "react-native";
+import { useDispatch, useSelector } from "react-redux";
+import { PayloadAction, Action } from "@reduxjs/toolkit";
+import { Dispatch } from "redux";
+import {
+  fetchAlbumsRequest,
+  fetchAlbumsSuccess,
+  fetchAlbumsFailure
+} from "./albumsSlice";
+import axios from "axios";
+import { RootState } from "../../redux/store";
 const Albums = () => {
+  const dispatch = useDispatch();
+  const fetchAlbums = async (): Promise<Action> => {
+    // const fetchAlbums = () => async (
+    //  dispatch: Dispatch<PayloadAction>
+    //): Promise<Action> => {
+    dispatch(fetchAlbumsRequest());
+    console.log('we"re loaing');
+    try {
+      const { data } = await axios.get(
+        "https://jsonplaceholder.typicode.com/albums/",
+        {
+          params: { _limit: 10 }
+        }
+      );
+      console.log("ALBUMS RECEIVED", data);
+      return dispatch(fetchAlbumsSuccess(data));
+    } catch (e) {
+      console.log("error");
+      return dispatch(fetchAlbumsFailure(e.message));
+    }
+  };
+
+  const albums = useSelector((state: RootState) => state.albums);
+  const loading = useSelector((state: RootState) => state.loading);
+  useEffect(() => {
+    fetchAlbums();
+  }, []);
   return (
     <View>
+      {loading && <ActivityIndicator size="large" />}
       <Text>ALBUMS HERE</Text>
     </View>
   );
