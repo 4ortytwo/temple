56c109afd1c5bd78e83398635b78a0902a2b0231
4ortytwo <burgan.arthur@gmail.com>
2020-02-29 12:58:39 +0100

    remove unused import

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Provider } from "react-redux";
-import store, { RootState } from "./src/redux/store";
+import store from "./src/redux/store";
 import { Routes } from "./src/Routes";
 
 export default function AppWrapper() {
